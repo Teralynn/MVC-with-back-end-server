9a74a96147c2c674110afbe162cbfbc334f9f136
Teralynn <daniel.hakansson.te11c@gmail.com>
2016-10-24 11:52:47 +0200

    Add files via upload

---
+++ IService1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WebService
+{
+  
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        public List<ItemClass> GetList(string type);
+
+        [OperationContract]
+        public List<ItemClass> GetAll();
+      
+    }
+
+
+    
+    /*
+    [DataContract]
+    public class CompositeType
+    {
+      
+    
+    }
+     * */
+}
